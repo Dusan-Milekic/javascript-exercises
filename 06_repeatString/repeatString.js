6ec443ff6ef88757aa143e0ae9f6ea4a326d683c
Dusan-Milekic <dusanmilekic0511@gmail.com>
2025-06-10 21:24:58 +0200

    Finished 6 exercieses

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(message,num) {
+    if (num == 0){return ''}
+    else if (num < 0){return 'ERROR'}
+    else{
+        r_message = '';
+        for (let i = 0; i < num; i++)
+            r_message += message
+    
+    }
 
+    return r_message;
 };
 
 // Do not edit below this line
